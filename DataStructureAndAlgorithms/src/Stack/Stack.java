7bf5d960fa2b377c852f61ce08c0e9af7ca16af1
Sagar-sagu <sagargk307@gmail.com>
2026-01-24 23:31:38 +0530

    5th commit

---
+++ DataStructureAndAlgorithms/src/Stack/Stack.java
@@ -0,0 +1,11 @@
+package src.Stack;
+
+import java.util.Scanner;
+
+public class Stack{
+  static Scanner scan = new Scanner(System.in);
+  public void CreateStack(){
+    System.out.println("Enter Stack Name");
+    String usrSuggestName = scan.nextLine();
+  }
+}
--- DataStructureAndAlgorithms/src/Stack/StackMenu.java
+++ DataStructureAndAlgorithms/src/Stack/StackMenu.java
@@ -7,5 +7,15 @@ public class StackMenu
     public static void StackOperations()
     {
       Scanner scan = new Scanner(System.in);
+
+      System.out.println("1.Create Stack Data Structure");
+      System.out.println("2.isEmpty (check wheather stack is empty or not)");
+      System.out.println("3.Push (add elements to stack)");
+      System.out.println("4.Pop (remove  top of the stack element )");
+      System.out.println("5.Peek (it returns top of the stack element)");
+      System.out.println("6.Search (check wheather Searching element is there in stack or not)");
+      System.out.println("7.Size (it returns size of stack)");
+      System.out.println("8.Back");
+      System.out.println("9.Exit");
     }
 }
